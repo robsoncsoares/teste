8bfda25a7ca393150a64330506958bdefeba5e4c
Robson de Carvalho Soares <robsoncsoares@gmail.com>
2014-11-27 21:06:30 -0200

    Update Teste.java

--- src/teste/Teste.java
+++ src/teste/Teste.java
@@ -3,8 +3,9 @@ package teste;
 public class Teste {
 
 	public static void main(String[] args) {
-		System.out.println("teste1");
+		System.out.println("teste4");
 		System.out.println("teste2");
+		System.out.println("teste3");
 		System.out.println("teste3");
 
 	}
